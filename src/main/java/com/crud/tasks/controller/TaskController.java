923143f6cacc0f7f3a9b7fd9166720d4c5f60b2a
Jan Sikorski <jans3876@gmail.com>
2023-01-29 22:23:14 +0100

    JDX-18.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,9 +1,7 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,23 +10,27 @@ import java.util.List;
 @RequestMapping("/v1/tasks")
 public class TaskController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "")
+    @GetMapping
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
+    @GetMapping
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
 
+    @DeleteMapping
     public void deleteTask(Long taskId) {
 
     }
 
+    @PutMapping
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
+    @PostMapping
     public void createTask(TaskDto taskDto) {
 
     }
